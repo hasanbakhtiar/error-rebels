6fe8baba3f11b7ac455aa6cc73a70642ac032e59
HasanBakhtiar <hasan.doctype@gmail.com>
2023-11-16 17:02:01 +0400

    lastest

---
+++ JavaScript/PureJS/lesson2-data-types/js/script.js
@@ -0,0 +1,133 @@
+
+// Primitive DT
+// number
+// string
+// boolean - (true,false)
+
+
+
+// Composite DT 
+// Array
+// var infoType = [5,"hello",true];
+// console.log(infoType);
+// object
+// var infoObj = {
+//     title:"Asus",
+//     price:300,
+//     newDevice:true
+// }
+
+// Special DT
+// undefined
+// null
+
+
+
+// OPERATORS 
+
+
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+var x = 5;
+// x %=10; //(x=x+10)
+// console.log(x);
+
+
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+// var x = 5 ;
+// x--;
+
+// console.log(x);
+
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+
+
+
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+
+if (!false) {
+    console.log('bu duzdur');
+}else{
+    console.log('bu sehvdir');
+}
+
+
+
+// string operators
+// +
+// +=
+
+
+
+// var infoOne = "Javascript";
+// infoOne += " Hello";
+
+// console.log(infoOne);
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5,  b= 10
+// console.log(a,b);
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var info = 'JavaScript\'s Book';
+// console.log(info); 
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+
+
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a = 5;
+
+
+
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+
+
