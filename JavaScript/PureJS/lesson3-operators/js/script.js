ef29a9a0fef49415fe84b93fe5a5c3eadbc5aae0
HasanBakhtiar <hasan.doctype@gmail.com>
2023-11-20 17:04:55 +0400

    var let const

---
+++ JavaScript/PureJS/lesson3-operators/js/script.js
@@ -0,0 +1,127 @@
+
+
+
+// OPERATORS 
+
+
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+// var a=20
+// a%=10
+// console.log(a);
+
+// var x = 5;
+// x %=10; //(x=x+10)
+// console.log(x);
+
+
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+// var x = 5 ;
+// x--;
+
+// console.log(x);
+
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+// var a = 10;
+// var b = 10;
+
+// if (a!==b) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+
+// if (!false) {
+//     console.log('bu duzdur');
+// }else{
+//     console.log('bu sehvdir');
+// }
+
+
+
+// string operators
+// +
+// +=
+
+
+
+// var infoOne = "Javascript";
+// infoOne += " Hello";
+
+// console.log(infoOne);
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5,b = 10;
+
+// console.log(a  ,b);
+
+
+// var a = "Hasan's Book";
+// console.log(a);
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+// var a  = [];
+// console.log(typeof a);
+
+
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
