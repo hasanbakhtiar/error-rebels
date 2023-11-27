feeaaf5d6177b2280d687fae5fbd3629954f97f5
HasanBakhtiar <hasan.doctype@gmail.com>
2023-11-27 17:20:49 +0400

    js loop

--- JavaScript/PureJS/lesson6-loops/js/script.js
+++ JavaScript/PureJS/lesson6-loops/js/script.js
@@ -1,13 +1,28 @@
 
-let count = 0;
-while(count <100){
-    count++;
-    if (count ==5) {
-        continue;
-    }
-    console.log(count);
-   
-    
-}
+// let count = 0;
+// while(count <100){
+//     count++;
+//     if (count ==5) {
+//         continue;
+//     }
+//     console.log(count);
+
+
+// }
 
 // count ++ | count = count+1
+
+
+// let count = 0;
+
+// do {
+
+//     count++;
+//     console.log(count);
+
+// } while (count < 10)
+
+
+for(let count=1; count<=10;count++){
+        console.log("JavaScript"+count);
+}
---
+++ JavaScript/PureJS/lesson7-array/js/script.js
@@ -0,0 +1,58 @@
+
+// const info = [1, 2, 3];
+// const infoTwo = new Array(3, 2, 1);
+
+
+// const info = [];
+// info[0] = 'Hello1'
+// info[1] = 'Hello2'
+// info[2] = 'Hello3'
+// info[3] = 'Hello4'
+
+// const info = [1,2,3];
+// info[3] = "Hello"
+// console.log(info);
+
+
+// const info = [1,2,3,[true,false,["Hello",2,['new value'],['data']]],4,5];
+// console.log(info[3][2][3][0]);
+
+
+const brand = ["Samsung","Apple","Xiaomi","OnePlus","Oppo","LG"];
+
+const laptop = ["Asus","Hp","Acer","MSI","Lenovo"];
+
+// brand.push("new brand1");
+// brand.unshift("new brand2");
+
+// brand.pop();
+// brand.shift();
+
+brand.push(...laptop);
+console.log(brand);
+
+
+
+// for (let i = 0; i < brand.length; i++) {
+//     console.log(brand[i]);
+// }
+
+// for(let a in brand){
+//     console.log(a);
+// }
+
+
+// for(let item of brand){
+//     console.log(item);
+// }
+
+
+
+
+
+
+
+
+
+
+
