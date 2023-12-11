9e2686745a8b2ee17f6a67cf6e1e612853db07f1
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-11 15:45:06 +0400

    DOM part1

--- JavaScript/PureJS/lesson11-DOM-part-1/js/script.js
+++ JavaScript/PureJS/lesson11-DOM-part-1/js/script.js
@@ -1,4 +1,24 @@
-// let a = "2"+3*5;
-// console.log(a);
+// DOM elemets selectors
+
+// document.getElementsByTagName('h1')[0].innerHTML = "Moko";
+// document.getElementById('demo').innerHTML = "Cilli";
+// fdhg
+
+
+// const myHeaderText = document.getElementsByTagName('h1');
+// console.log(myHeaderText.length);
+
+
+// for (let i = 0; i < myHeaderText.length; i++) {
+//     myHeaderText[i].innerHTML = "Tiko"; 
+    
+// }
+
+
+
+// const myHeaderText = document.querySelector('#demo');
+// const myHeaderText = document.querySelectorAll('.test')[1];
+
+// myHeaderText.innerHTML = 'miko';
 
 
