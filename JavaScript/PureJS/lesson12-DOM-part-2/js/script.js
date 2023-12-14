e1c0c2a324d3d68e9861433c06bd0438c4dde8fb
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-14 15:50:36 +0400

    DOM part 2

---
+++ JavaScript/PureJS/lesson12-DOM-part-2/js/script.js
@@ -0,0 +1,58 @@
+// console.log(document);
+// text.innerText = "<h1>Hello</h1>";
+
+
+const text = document.querySelectorAll('p');
+
+
+
+document.querySelector('button').onclick=()=>{
+
+
+
+    // console.log(text[3].className);
+    
+    
+    
+    
+    // document.querySelector('button').classList.toggle('az');
+
+    // if (document.querySelector('button').className == "az") {
+    //     for (let i = 0; i < text.length; i++) {
+    //         text[i].innerHTML = "Salam"
+    //     }
+    // }else{
+    //     for (let i = 0; i < text.length; i++) {
+    //         text[i].innerHTML = "Hello"
+    //     }
+    // }
+
+    // text.classList.remove('red');
+    // text.classList.add('green');
+    // text.classList.toggle('green');
+
+    
+    
+    // if (text.attributes[0].value == 'red') {
+    //     text.attributes[0].value = "green";
+    // }else{
+    //     text.attributes[0].value = "red";
+    // }
+}
+
+
+
+// document.querySelector('button').onclick = () => {
+
+
+//     // text.style.backgroundColor = "black";
+//     // text.style.color = "red";
+//     // text.style.fontStyle = "italic";
+
+//     // if (text.style.color === "black") {
+//     //     text.style.color = "red";
+//     // }else{
+//     //     text.style.color = "black"
+//     // }
+// }
+
