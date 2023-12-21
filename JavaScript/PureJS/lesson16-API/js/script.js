f7517b818d2d903a3bf2bd4c0044e67c40a3d907
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-21 17:02:10 +0400

    lastest

--- JavaScript/PureJS/lesson16-API/js/script.js
+++ JavaScript/PureJS/lesson16-API/js/script.js
@@ -58,7 +58,12 @@
 //     ul.innerHTML = li;
 // })
 
-
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+setTimeout(() => {
+    h1.style.display = "block";
+    p.style.display = "none";
+}, 3000);
 
 
 
