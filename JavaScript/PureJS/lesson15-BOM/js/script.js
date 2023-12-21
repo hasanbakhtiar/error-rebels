e79ec4a219dfc6e5413b1c845429db0d49f349db
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-21 16:45:20 +0400

    lastest

---
+++ JavaScript/PureJS/lesson15-BOM/js/script.js
@@ -0,0 +1,14 @@
+
+
+// 1saniye = 1000 ms 
+// setInterval(()=>{
+//  const info  = new Date();
+// document.body.innerHTML = info;
+// },1000);
+
+
+// document.body.innerHTML = window.navigator.platform;
+
+// document.querySelector("button").onclick=()=>{
+//   window.open("https://google.com","", "width=1200,height=1100")
+// }
