f1998e57241acdf3ce3c30dbc3854421009f20fe
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-25 17:06:55 +0400

    api

--- JavaScript/PureJS/lesson17-API-part2/js/script.js
+++ JavaScript/PureJS/lesson17-API-part2/js/script.js
@@ -19,7 +19,7 @@
 
 
 
-const ul = document.querySelector('ul');
+const row = document.querySelector('.row');
 
 const fetchApi = () => {
     fetch("https://fakestoreapi.com/products")
@@ -27,9 +27,42 @@ const fetchApi = () => {
         .then(data => {
             let li = "";
             data.map(cd=>{
-                li+=`<li>${cd.title}</li>`
-            })
-            ul.innerHTML = li;
+                li+=`<div class="col-12 col-sm-6 col-md-4">
+                <div class="card">
+                    <img src="${cd.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${cd.title}</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>`
+            });
+            row.innerHTML = li;
         })
 } 
 fetchApi()
+
+
+
+// const ul = document.querySelector('ul');
+
+// const fetchApi = () => {
+//     fetch("https://fakestoreapi.com/products")
+//         .then(res => res.json())
+//         .then(data => {
+//             let li = "";
+//             const a = [];
+//             data.map(cd=>{
+//                 // li+=`<li>${cd.title}</li>`
+//                 a.push(cd.title)
+//             });
+//             console.log(a.sort());
+//             const linew = document.querySelectorAll('li');
+//             for(let b in a.sort()){
+//                 linew[b].innerHTML = a[b];
+//             }
+//             ul.innerHTML = li;
+//         })
+// } 
+// fetchApi()
