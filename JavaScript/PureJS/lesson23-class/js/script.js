be4c992a3899d38a768c17d73920db765be53be7
HasanBakhtiar <hasan.doctype@gmail.com>
2024-01-15 18:56:33 +0400

    enter react

--- JavaScript/PureJS/lesson23-class/js/script.js
+++ JavaScript/PureJS/lesson23-class/js/script.js
@@ -11,6 +11,18 @@ class Car{
 }
 
 
+class Moto extends Car{
+    constructor(model,year,price){
+        super(model,year,price)
+    }
+}
+
+
+
 const myCar = new Car("Bmw X5",2000);
 console.log(myCar.infomodel);
 console.log(myCar.showData());
+
+
+const myMoto = new Moto("Yamaha",1995);
+console.log(myMoto.showData());
