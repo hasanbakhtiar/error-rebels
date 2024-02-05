1e582bf94256df3297f1302a7b582e26b6ae0929
Hasan <hasan.doctype@gmail.com>
2024-02-05 15:55:30 +0400

    react router dom

--- JavaScript/ReactJS/lesson10-router/src/App.jsx
+++ JavaScript/ReactJS/lesson10-router/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './page/Home';
+import About from './page/About';
+import NotFoundPage from './page/NotFoundPage';
+import Header from './Components/Header';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header />  
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='*' element={<NotFoundPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
