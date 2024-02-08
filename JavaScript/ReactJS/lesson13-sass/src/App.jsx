0bbe2b4471ed06192e8e6b292ecd7797e1825f39
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-02-08 17:53:23 +0400

    useparams

---
+++ JavaScript/ReactJS/lesson13-sass/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const App = () => {
+  return (
+ <>
+    <div className='con'>
+        <div className="box-one">Hello</div>
+        <div className="box-two"></div>
+        <div className="box-three"></div>
+    </div>
+
+    <ul>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+        <li>test list</li>
+    </ul>
+ </>
+  )
+}
+
+export default App
