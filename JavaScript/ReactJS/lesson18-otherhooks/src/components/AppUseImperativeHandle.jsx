134359e95229e05f7455b60e38f3a03929b2edea
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-04-15 17:03:45 +0400

    updated

--- JavaScript/ReactJS/lesson18-otherhooks/src/components/AppUseImperativeHandle.jsx
+++ JavaScript/ReactJS/lesson18-otherhooks/src/components/AppUseImperativeHandle.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
+import { useRef } from 'react';
+import { useState } from 'react';
+import { useImperativeHandle } from 'react';
+import { forwardRef } from 'react';
 import {Helmet} from "react-helmet";
 
-const MyInput = ()=>{
-  return(
-    <button>send</button>
-  )
-} 
+// const MyInput = forwardRef((props,ref)=>{
+//   useImperativeHandle(ref,()=>({
+//     hi:sayhi
+//   }))
+  
+  
+//   const sayhi = ()=>{
+//     alert('Hello')
+//   }
+  
+//   return(
+//     <div><input type="text" /></div>
+//   )
+// } )
+
+
+
+
+
 
 const AppUseImperativeHandle = () => {
-  
+  // const inputRef = useRef();
+  // const handleClick = ()=>{
+  //   inputRef.current.hi()
+  // }
+  const [data,setData] = useState("");
+  const inputRef = useRef();
   return (
     <div className="application">
-    <Helmet>
+    {/* <Helmet>
         <meta charSet="utf-8" />
         <title>AppUseImperativeHandle</title>
     </Helmet>
     
-    <MyInput />
-    
+    <div className="d-flex">
+    <MyInput ref={inputRef} /><button onClick={handleClick}>send</button>
+    </div> */}
+
+
+    {/* {inputRef} */}
+    <input type="text" ref={inputRef} /><button onClick={()=>{setData(inputRef.current.value)}}>test</button>
+      {data}
 </div>
   )
 }
